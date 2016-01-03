38f4d5b24d0546ef5cb6c0c9f364f3c6ece00e55
laitilee <tiffanyjlai@gmail.com>
2016-01-02 23:46:18 -0800

    Moving to Cascade Mall
    Updated store hours and address

--- js/map.js
+++ js/map.js
@@ -1,15 +1,15 @@
 function initialize() {
   var mapOptions = {
-    center: { lat: 48.431884, lng: -122.320315},
-    zoom: 14
+    center: { lat: 48.462191, lng: -122.338790},
+    zoom: 15
   };
   var map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
 
-  var mountVernon = new google.maps.LatLng(48.431884, -122.320315);
+  var burlington = new google.maps.LatLng(48.462191, -122.338790);
 
   var marker = new google.maps.Marker({
-    position: mountVernon,
+    position: burlington,
     map: map,
     title:"Mount Vernon"
   });
